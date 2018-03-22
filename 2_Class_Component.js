caec35a8804043cf5168bd68e9442d53ecce7a1c
Manan Sheth <manan.sheth@aeris.net>
2018-03-22 16:06:32 -0500

    Removing constructor in Class component

--- 2_Class_Component.js
+++ 2_Class_Component.js
@@ -1,10 +1,5 @@
 class Button extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            counter: 0
-        };
-    }
+    state = {counter:0};
 
     render() {
         return (<button> {this.state.counter} </button>);
