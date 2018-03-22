86ed45539592c5634eea628192cd68f8026f7354
Manan Sheth <manan.sheth@aeris.net>
2018-03-22 17:37:18 -0500

    3 Multiple Components , working with changed scope

--- 3_Multiple_Components.js
+++ 3_Multiple_Components.js
@@ -1,26 +1,38 @@
 class Button extends React.Component {
-    state = {counter:0};
-    handleClick = () => {
-        this.setState((prevState)=>{
-            counter : prevState.counter +1
-        });
-    };
+ 
+    // handleClick = () => {
+    //     this.setState((prevState)=>({
+    //         counter : prevState.counter +1
+    //     }));
+    // };
 
     render(){
-        return (<button onClick={this.handleClick}>{this.state.counter}</button>);
+        return (
+            <button onClick={this.props.onClickFunction}>
+            +1
+            </button>
+        );
     }
 }
 
 const Result = (props) => {
-    return (<div>...</div>);
+    return (<div>{props.counter}</div>);
 };
 
 class App extends React.Component {
+    state = { counter: 0 };
+    
+    incrementCounter = () =>{
+        this.setState((prevState) => ({
+            counter : prevState.counter +1
+        }));
+    };
+
     render(){
         return (
             <div>
-                <Button/>
-                <Result/>
+                <Button onClickFunction={this.incrementCounter}/>
+                <Result counter = {this.state.counter}/>
             </div>
         );
     }
