6319369c7f7eeeadfa48c020c08c7bc2d66826f6
Manan Sheth <manntsheth@gmail.com>
2018-03-24 12:40:50 -0500

    4_One GitHub Card Working

--- 4_Card_Github.js
+++ 4_Card_Github.js
@@ -2,15 +2,21 @@ const Card = (props) => {
     const innerDivStyle = { display: 'inline-block', marginLeft: 15, color: 'red' }
     return (
         <div style={{ margin: '1em' }}>
-            <img src="http://placehold.it/75" />
+            < img width="75" src = "https://avatars3.githubusercontent.com/u/7351847?v=4" / >
             <div style={innerDivStyle}>
                 <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>Name here...</div>
-                <div>Company Name here...</div>
+                <div>Manan</div>
             </div>
         </div>
     );
 };
 
+const CardList = (props) => {
+    return(
+        <div>
+            <Card/>
+        </div>
+    );
+}
 
-
-ReactDOM.render(<Card />, mountNode);
+ReactDOM.render(<CardList />, mountNode);
