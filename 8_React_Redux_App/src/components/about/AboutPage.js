1a880521974d6700ffeae138a441619c3aebdbfb
Manan Sheth <manntsheth@gmail.com>
2018-05-10 01:06:29 -0500

    8_React_Redux_App Basic structure with webpack,npm scripts, eslint, babel, editorconfig, mocha, react. Redux remaining

---
+++ 8_React_Redux_App/src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>About </h1>
+                <p> This app uses react, redux, router etc etc aacaataaaaab</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
